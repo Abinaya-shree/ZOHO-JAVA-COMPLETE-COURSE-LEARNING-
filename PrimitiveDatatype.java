7399108bc6ca243c3dc4932d17200fb5948f233a
Abinaya Shree M S <109414932+Abinaya-shree@users.noreply.github.com>
2026-04-17 17:46:35 +0530

    Add files via upload

---
+++ PrimitiveDatatype.java
@@ -0,0 +1,21 @@
+public class PrimitiveDatatype{
+    public static void main(String[] args){
+          byte a=9;
+          short b=100;
+          int c=2456;
+          float d=2.345f;
+          double e=2.567890;
+          long f=123457903;
+          boolean g=true;
+          char h='a';
+          System.out.println("Byte "+a);
+          System.out.println("Short "+b);
+          System.out.println("Integer "+c);
+          System.out.println("Float "+d);
+          System.out.println("Double "+e);
+          System.out.println("Long "+f);
+          System.out.println("Boolean "+g);
+          System.out.println("Char "+h);
+
+    }
+}
