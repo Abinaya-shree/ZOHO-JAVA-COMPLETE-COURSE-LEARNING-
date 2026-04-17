f836d82c8a6897808b8f22eb7d551193ab33c786
Abinaya Shree M S <109414932+Abinaya-shree@users.noreply.github.com>
2026-04-17 16:46:23 +0530

    Add files via upload

---
+++ VariableDeclaration.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class VariableDeclaration {
+    public static void main(String[] args) {
+        int num;    //declaration
+        num=10;    //intialization
+        System.out.println(num);
+    }
+}
